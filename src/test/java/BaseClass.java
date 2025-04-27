8a462de97f30af1b193f1ae7d6ef8194d170c7c2
jananirj94 <jananirj1794@gmail.com>
2025-04-27 21:35:38 +0530

    message

--- src/test/java/BaseClass.java
+++ src/test/java/BaseClass.java
@@ -1,4 +1,12 @@
 
 public class BaseClass {
-
+	private void method7() {
+		System.out.println("method7");
+	}
+	private void method8() {
+		System.out.println("method8");
+	}
+	private void method9() {
+		System.out.println("method9");
+	}
 }
